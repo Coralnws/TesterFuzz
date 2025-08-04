9154fd581c70118b24ae68fc3f44898b9ca8065f
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 23:04:03 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/testerfuzz/main.go
@@ -0,0 +1,23 @@
+// cmd/testerfuzz/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"testerfuzz/internal/testerfuzz"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := testerfuzz.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
